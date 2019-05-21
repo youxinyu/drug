75c3592eb0207a2382e9457f058b202c43124ac6
xiaowu <xiaowu@qq.com>
2019-05-21 10:57:00 +0800

    123

--- src/main/java/com/guigu/drug/DrugApplication.java
+++ src/main/java/com/guigu/drug/DrugApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DrugApplication {
 
     public static void main(String[] args) {
+        int i=1;
         SpringApplication.run(DrugApplication.class, args);
     }
 
