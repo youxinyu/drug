1f792f6f1e18fdfb06aecfc382652eace3afcdfe
xiaowu <xiaowu@qq.com>
2019-05-21 11:12:47 +0800

    qwe

--- src/main/java/com/guigu/drug/DrugApplication.java
+++ src/main/java/com/guigu/drug/DrugApplication.java
@@ -8,6 +8,7 @@ public class DrugApplication {
 
     public static void main(String[] args) {
         int i=1;
+        int j=0;
         SpringApplication.run(DrugApplication.class, args);
     }
 
